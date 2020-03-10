af5f72b8b674c3805d788cd2ed894d1e187eb868
danielvebo <danielvebo@gmail.com>
2020-03-09 22:13:14 -0500

    4 repo

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,34 +1,39 @@
 class Api::V1::ProductsController < ApplicationController
   protect_from_forgery with: :null_session
 
+
   def index
-    render json: Product.all
+    @products = Product.all
+    render json: { products: @products }
+  end
+
+  def new
+    @product = Product.new
   end
 
   def create
-    @product = Product.new(product_params)
-    if @product.save
-      render json: @product, status: 201
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
+       @product = Product.new(prorams)
+       if @product.save
+         render json: @product, status: 201
+       else
+         render json: { errors: @product.errors }, status: 422
+       end
+   end
 
-  end
 
-
-  def update
-    @product = Product.find(params[:id])
-    if @product.update(product_params)
-      render json: @product, status: 200
-    else
-      render json: @product.errors, status: 422
-    end
-  end
+   def update
+         @product = Product.find(params[:id])
+         if @product.update(product_params)
+           render json: @product, status: 200
+         else
+           render json: { errors: @product.errors }, status: 422
+         end
+     end
 
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
-    render json: @product, status: 204
+    head 204
   end
 
   private
